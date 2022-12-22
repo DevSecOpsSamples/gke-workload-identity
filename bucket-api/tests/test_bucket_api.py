74ab9214e525b9259dde56542ca2b78e4c008c4c
engel80 <prettyhip@gmail.com>
2022-12-23 02:20:23 +0900

    .

--- bucket-api/tests/test_bucket_api.py
+++ bucket-api/tests/test_bucket_api.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import unittest
 from unittest import mock
 
@@ -6,6 +7,10 @@ import main
 
 
 class RestAPIsTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        logging.basicConfig(level=logging.DEBUG)
 
     def test_root(self):
         response = main.app.test_client().get("/")
@@ -15,18 +20,18 @@ class RestAPIsTestCase(unittest.TestCase):
         response = main.app.test_client().get("/ping")
         self.assertEqual(response.status_code, 200)
 
-    # def test_bucket_invalid_bucket_name(self):
-    #     with self.assertRaises(ValueError):
-    #         main.app.test_client().get("/bucket")
+    def test_bucket_invalid_bucket_name(self):
+        with self.assertRaises(ValueError):
+            main.app.test_client().get("/bucket")
 
-    @mock.patch.dict(os.environ, {"GCS_BUCKET_NAME": "project-id-bucket-api"}, clear=True) 
+    @mock.patch.dict(os.environ, {"GCS_BUCKET_NAME": "project-id-372417-bucket-api", "GOOGLE_CLOUD_PROJECT":"project-id-372417"}, clear=True) 
     def test_bucket_invalid_bucket_name(self):
         response = main.app.test_client().get("/bucket")
-        self.assertEqual(response.status_code, 200, 'response : %s' % response.data)
+        self.assertEqual(response.status_code, 500, 'response : %s' % response.data)
 
-    @mock.patch.dict(os.environ, {"GCS_BUCKET_NAME": "project-id-bucket-api"}, clear=True) 
+    @mock.patch.dict(os.environ, {"GCS_BUCKET_NAME": "project-id-372417-bucket-api"}, clear=True)
     def test_env(self):
-        self.assertEqual(os.environ.get("GCS_BUCKET_NAME"), "project-id-bucket-api")
+        self.assertEqual(os.environ.get("GCS_BUCKET_NAME"), "project-id-372417-bucket-api")
 
     def test_credential(self):
         print('GOOGLE_CLOUD_PROJECT: {}'.format(os.environ.get('GOOGLE_CLOUD_PROJECT')))
