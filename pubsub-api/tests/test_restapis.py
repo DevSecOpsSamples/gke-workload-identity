5b9aa54713a1b25423349830b457efe1d76f4628
engel80 <prettyhip@gmail.com>
2022-12-22 19:34:46 +0900

    .

---
+++ pubsub-api/tests/test_restapis.py
@@ -0,0 +1,28 @@
+import os
+import logging
+import unittest
+from unittest import mock
+from test import support
+
+import main
+
+
+class RestAPIsTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        logging.basicConfig(level=logging.DEBUG)
+
+    def test_ping(self):
+        response = main.app.test_client().get("/ping")
+        self.assertEqual(response.status_code, 200, 'response : %s' % response.data)
+
+    def test_bucket_invalid_bucket_name(self):
+        """ GCS_BUCKET_NAME is None """
+        with self.assertRaises(ValueError) as e:
+            main.app.test_client().get("/bucket")
+
+    @mock.patch.dict(os.environ, {"GCS_BUCKET_NAME": "PROJECT-ID-bucket-api"}, clear=True) 
+    def test_bucket_invalid_bucket_name(self):
+        response = main.app.test_client().get("/bucket")
+        self.assertEqual(response.status_code, 500, 'response : %s' % response.data)
