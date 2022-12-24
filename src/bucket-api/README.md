ea3b13abd70b5f0d5d6fa99b46fa0833d6e4b997
engel80 <prettyhip@gmail.com>
2022-12-24 17:42:01 +0900

    .

---
+++ src/bucket-api/README.md
@@ -0,0 +1,6 @@
+#
+
+```bash
+python3 -m venv env
+source env/bin/activate
+```
--- src/bucket-api/tests/test_bucket_api.py
+++ src/bucket-api/tests/test_bucket_api.py
@@ -16,18 +16,19 @@ class RestAPIsTestCase(unittest.TestCase):
         response = main.app.test_client().get("/ping")
         self.assertEqual(response.status_code, 200)
 
-    def test_bucket_invalid_bucket_name(self):
-        with self.assertRaises(ValueError):
-            main.app.test_client().get("/bucket")
-
-    def test_bucket_invalid_bucket_name(self):
-        response = main.app.test_client().get("/buckets")
-        self.assertEqual(response.status_code, 200, 'response : %s' % response.data)
-
-    def test_bucket_invalid_bucket_name(self):
-        response = main.app.test_client().get("/bucket")
-        self.assertEqual(response.status_code, 200, 'response : %s' % response.data)
-
     @mock.patch.dict(os.environ, {"GCS_BUCKET_NAME": "project-id-bucket-api"}, clear=True)
     def test_env(self):
         self.assertEqual(os.environ.get("GCS_BUCKET_NAME"), "project-id-bucket-api")
+
+    @mock.patch.dict(os.environ, {}, clear=True)
+    def test_bucket_invalid_bucket_name(self):
+        with self.assertRaises(ValueError):
+            main.bucket
+
+    def test_buckets(self):
+        response = main.app.test_client().get("/buckets")
+        self.assertEqual(response.status_code, 200, 'response : %s' % response.data)
+
+    def test_bucket(self):
+        response = main.app.test_client().get("/bucket")
+        self.assertEqual(response.status_code, 200, 'response : %s' % response.data)
